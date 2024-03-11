1dac70969f1191e28b8daa194ae9aeb822c8b67a
annanas-dev <123464405+annanas-dev@users.noreply.github.com>
2024-03-11 23:24:10 +0300

    создала  приложение работающее с двумя потоками

---
+++ Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_4_OS
+{
+    public class BackgroundWorker
+    {
+        public void Execute()
+        {
+            new Thread(() =>
+            {
+                while (true) 
+                {
+                    Console.WriteLine("Фоновый поток работает...");
+                    Thread.Sleep(1000);
+                }
+            })
+            { IsBackground = true }.Start();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_4_OS
+{
+    public partial class Form1 : Form
+    {
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker.Execute(); 
+            StartMainWork(); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartMainWork()
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    this.Invoke(new Action(() =>
+                    {
+                        Text = $"Основной поток: {DateTime.Now}";
+                    }));
+                    Thread.Sleep(1000); 
+                }
+            })
+            { IsBackground = true }.Start(); 
+        }
+    }
+}
